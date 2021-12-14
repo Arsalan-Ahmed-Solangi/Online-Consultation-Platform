54e0661cd281fa3e5a164c70894f1c1797d14941
Arsalan Ahmed <ahmedsolangi347@gmail.com>
2021-12-15 02:11:11 +0500

    Dashboard Modified
    Dashboard Modified

---
+++ application/views/Admin/navigation.php
@@ -0,0 +1,42 @@
+<nav class="header-nav ms-auto">
+      <ul class="d-flex align-items-center">
+
+        <li class="nav-item dropdown pe-3">
+
+          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+            <img src="<?=base_url()?>/assets/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+          </a><!-- End Profile Iamge Icon -->
+
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+            <li class="dropdown-header">
+              <h6>Kevin Anderson</h6>
+              <span>Web Designer</span>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                <i class="bi bi-person"></i>
+                <span>My Profile</span>
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            
+
+           
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="#">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Sign Out</span>
+              </a>
+            </li>
+
+          </ul><!-- End Profile Dropdown Items -->
+        </li><!-- End Profile Nav -->
+
+      </ul>
+    </nav>
--- application/views/Includes/footer.php
+++ application/views/Includes/footer.php
@@ -1,11 +1,25 @@
 
+  
+	<!-- Start of Back To Top -->
+	<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+	<!-- End of Back To Top -->
 
+ <!-- Start of Footer -->
+  <footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Developers 2021</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+     
+      Designed by Arsalan & Adeeb
+    </div>
+  </footer>
+	<!-- End of Footer -->
 
-<!-- Start of Javascript-->
+	<!-- Start of Javascript-->
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script> -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script> -->
 
   <!-- Vendor JS Files -->
   <script src="<?=base_url()?>/assets/assets/vendor/apexcharts/apexcharts.min.js"></script>
@@ -21,14 +35,14 @@
   <script src="<?=base_url()?>/assets/assets/js/main.js"></script>
 
 
-<script type="text/javascript">
-	$(document).ready(function(){
+	<script type="text/javascript">
+		$(document).ready(function(){
 
-		$("#form").validate();
-		$('.error').delay(2000).fadeOut('slow');
-	});
-</script>
-<!-- End of Javascript -->
+			$("#form").validate();
+			$('.error').delay(2000).fadeOut('slow');
+		});
+	</script>
+	<!-- End of Javascript -->
 	
 </body>
 </html>
